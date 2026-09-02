e8e815c5579fdfe16bb6b6027ed0cd774b3410eb
Nicolas Freches <freches@instmath.rwth-aachen.de>
2026-09-02 19:08:46 +0200

    Added reciprocal rank evaluation

--- src/evaluate_bm25.py
+++ src/evaluate_bm25.py
@@ -44,6 +44,8 @@ def main():
         for qrel in qrels
     }
 
+    reciprocal_ranks = []
+
     for query_record in queries:
         query_id = query_record["query_id"]
 
@@ -92,74 +94,35 @@ def main():
             judgments[arxiv_id] == 1
             for arxiv_id in retrieved_ids
         )
+        #####RR########
+        first_relevant_rank = next(
+        (
+            rank
+            for rank, arxiv_id in enumerate(retrieved_ids, start=1)
+            if judgments[arxiv_id] == 1
+        ),
+        None,
+        )
 
-        precision_at_k = relevant_retrieved / K
+        if first_relevant_rank is None:
+            reciprocal_rank = 0.0
+        else:
+            reciprocal_rank = 1 / first_relevant_rank
+
+        reciprocal_ranks.append(reciprocal_rank)
+        ###############
+
+        reciprocal_rank = 1 / first_relevant_rank
+        reciprocal_ranks.append(reciprocal_rank)
 
         print(f"Relevant retrieved: {relevant_retrieved}/{K}")
         print(f"Precision@{K}: {precision_at_k:.3f}")
+        print(f"RR@{K}: {reciprocal_rank:.3f}")
         print()
 
-# def main():
-#     papers = load_jsonl(CORPUS_FILE)
-#     queries = load_jsonl(QUERIES_FILE)
-#     qrels = load_jsonl(QRELS_FILE)
+    mrr_at_k = sum(reciprocal_ranks) / len(reciprocal_ranks)
+    print(f"MRR@{K}: {mrr_at_k:.3f}")
 
-#     query_record = next(
-#         query for query in queries
-#         if query["query_id"] == QUERY_ID
-#     )
-
-#     query_text = query_record["query"]
-
-#     relevant_ids = {
-#         qrel["arxiv_id"]
-#         for qrel in qrels
-#         if qrel["query_id"] == QUERY_ID
-#         and qrel["relevance"] == 1
-#     }
-
-#     documents = [
-#         paper["title"] + " " + paper["abstract"]
-#         for paper in papers
-#     ]
-
-#     corpus_tokens = bm25s.tokenize(
-#         documents,
-#         stopwords=None,
-#         stemmer=None,
-#     )
-
-#     retriever = bm25s.BM25(method="lucene")
-#     retriever.index(corpus_tokens)
-
-#     query_tokens = bm25s.tokenize(
-#         query_text,
-#         stopwords=None,
-#         stemmer=None,
-#     )
-
-#     results, scores = retriever.retrieve(
-#         query_tokens,
-#         k=K,
-#     )
-
-#     retrieved_ids = [
-#         papers[document_index]["arxiv_id"]
-#         for document_index in results[0]
-#     ]
-
-#     relevant_retrieved = sum(
-#         arxiv_id in relevant_ids
-#         for arxiv_id in retrieved_ids
-#     )
-
-#     precision_at_k = relevant_retrieved / K
-
-#     print(f"Query: {query_text}")
-#     print(f"Relevant retrieved: {relevant_retrieved}/{K}")
-#     print(f"Precision@{K}: {precision_at_k:.3f}")
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
 
